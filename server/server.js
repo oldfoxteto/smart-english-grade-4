12a1635e0242449d23461289be1a094fe8b34528
oldfoxteto <sabrytamer8@gmail.com>
2026-03-22 00:11:01 +0200

    Rename product metadata to BeeFluent

--- server/server.js
+++ server/server.js
@@ -23,6 +23,7 @@ const DEFAULT_CLIENT_URLS = [
   'http://127.0.0.1:5173',
   'http://localhost:5174',
   'http://127.0.0.1:5174',
+  'https://beefluent.web.app',
   'https://ai-english-master.web.app',
   'https://ai-english-master.firebaseapp.com',
   'https://smart-english-grade-4.vercel.app',
